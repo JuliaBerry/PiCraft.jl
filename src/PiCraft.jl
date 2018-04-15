7d189c9185aa97f6329719b94aa699dddce6069f
Ellipse0934 <adityapuranik97@gmail.com>
2018-04-15 23:52:10 +0530

    Get Block Data + Expand function definition (#9)
    * small improvements
    
    * reverted playerpos type
    
    * fix
    
    * fix setPos()
    
    setPos() does not have any output from minecraft
    
    * change getPos() to Array from tuple
    
    * blocks.md
    
    Information table of Blocks
    
    * Get Block data + expand function template
    
    * Delete blocks.md

--- src/PiCraft.jl
+++ src/PiCraft.jl
@@ -4,8 +4,6 @@ include("blocks.jl")
 
 export world, player, chat, Block, find_item
 
-# package code goes here
-
 type World
     s::TCPSocket
     function World()
@@ -28,21 +26,23 @@ end
 
 module world
     using PiCraft
-    function getBlock(x, y, z)
-        (x, y, z) = (floor(Int64, x), floor(Int64, y), floor(Int64,z))
-        res = PiCraft.mc_send("world.getBlock($x,$y,$z)", true)
-        @assert length(res) == 1
-        return Block(parse(Int, res[1]))
+
+    function getBlock(pos::Tuple{Real, Real, Real})
+        pos = floor.(Int64, pos)
+        res = PiCraft.mc_send("world.getBlockWithData($(pos[1]),$(pos[2]),$(pos[3]))", true)
+        return Block(parse(Int, res[1]), (parse(Int, res[2])))
     end
 
-    function setBlock(x,y,z,block::PiCraft.Block)
-        PiCraft.mc_send("world.setBlock($x,$y,$z,$(block.id), $(block.data))", false)
+    getBlock(x, y, z) = getBlock((x,y,z))
+
+    function setBlock(pos::Tuple{Real, Real, Real},block::PiCraft.Block)
+        pos = floor.(Int, pos)
+        PiCraft.mc_send("world.setBlock($(pos[1]),$(pos[2]),$(pos[3]),$(block.id), $(block.data))", false)
     end
 
-    function setBlock(x,y,z,block::PiCraft.Block, data)
-        (x, y, z) = (floor(Int64, x), floor(Int64, y), floor(In64, z))
-        PiCraft.mc_send("world.setBlock($x,$y,$z,$(block.id),$data)", false)
-    end
+    setBlock(pos::Tuple{Real, Real, Real},block::PiCraft.Block, data) = setBlock(pos,Block(block.id, data))
+    setBlock(x,y,z,block::PiCraft.Block) = setBlock((x,y,z), Block)
+    setBlock(x,y,z,block::PiCraft.Block, data) = setBlock((x,y,z),Block(block.id, data))
 
     function setBlocks(x1,y1,z1,x2,y2,z2,block::PiCraft.Block)
         (x1, y1, z1, x2, y2, z2) = (floor(x1), floor(y1), floor(z1), floor(x2), floor(y2), floor(z2))
@@ -92,7 +92,6 @@ module player
     end
 
     function setPos(xf::Float64, yf::Float64, zf::Float64)
-
         PiCraft.mc_send("player.setPos($xf,$yf,$zf)", false)
     end
 
@@ -102,13 +101,11 @@ function mc_send(cmd, output=true)
     if minecraftWorld.s.status == Base.StatusInit || minecraftWorld.s.status == Base.StatusUninit
         error("Connection to Minecraft World is not initialised. Use `PiCraft.connectToWorld()` first.")
     end
-    #println("Command: $cmd")
     write(minecraftWorld.s, cmd*"\n")
-    #write(minecraftWorld.s, '\n')
     if output
         s = readline(minecraftWorld.s)
-        println("Return: $s")
         if contains(s,  "Fail")
+            println("Return: $s")
             error("Error from Minecraft in command $cmd")
         end
         return split(strip(s), ',')
