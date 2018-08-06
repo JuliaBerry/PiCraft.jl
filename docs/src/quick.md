7f168fa9bd8fd14050ce9e03d2af553067de443e
Ellipse0934 <adityapuranik97@gmail.com>
2018-08-06 18:38:28 +0530

    docs changes + Platform 9*3/4 Example (#23)
    * Added writeNBT, cut/copyModel, drawLine.width and readme changes
    
    - writeNBT is successfully writing
    - small changes to readme
    - cut/copy model functions in buildTools
    - added width to drawLine and corresponding changes in turtle
    
    * docs changes + Platform 9*3/4 Example
    
    * model flip and rotate functions
    * `post` function splats input args
    * Platform 9*3/4 example
    * Minor changes to docs

--- docs/src/quick.md
+++ docs/src/quick.md
@@ -9,7 +9,9 @@ You can save and restore your progress using the commands:
 1) Save: `save()`
 2) Load last savepoint: `restore()`
 
-A 3-D coordinate system is used to describe the Minecraft world. Throughout this package we describe a set of coordinates using a `Tuple` of numbers, for example `(3.5, 19, 7)` describes a position in the Minecraft World. The 2<sup>nd</sup> attribute of the `Tuple` describes the y-axis or height. When in game it is convenient to find the player coordinates by opening the debug menu.  
+A 3-D coordinate system is used to describe the Minecraft world. Throughout this package we describe a set of coordinates using a `Tuple` of numbers, for example `(3.5, 19, 7)` describes a position in the Minecraft World. The 2nd attribute of the `Tuple` describes the y-axis or height. When in game it is convenient to find the player coordinates by opening the debug menu.
+
+![](./assets/img/coordinates.png)
 
 ### Player
 You can find and set your player's location:
@@ -30,11 +32,23 @@ end
 ```
 The `id` defines the type of block (like cobblestone, wool, wood,.etc) and the `data` attribute defines additional characteristics of the block, on default every block has it `data` set to `0`. For example, Wool's block id is 35, `Block(35,0)` refers to a block of white wool. Different wool colors can be accessed by changing the `data` attribute. Red Wool is `Block(35, 14)`, Pink Wool is `Block(35,6)` and so on. A complete reference can be found in the documentation.
 
-1) Place Block: `setBlock(Tuple{Int,Int,Int}, block::Picraft.Block)`
+1) Place Block: `setBlock(Tuple{Int,Int,Int}, block::Block)`
     * Place the specified Block at the specified coordinates.
-2) Place Blocks: `setBlocks(pos::Tuple{UnitRange{Int}, UnitRange{Int}, UnitRange{Int}}, block::PiCraft.Block)`
-    * Place the specified block in the entire region described by the unit range.
+2) Place Blocks: `setBlocks(p1::Tuple{Real, Real, Real}, p2::Tuple{Real, Real, Real}, block::Block)`
+    * Set an entire region to the specified block type defined by the corners `p1` and `p2`.
 3) Get Block information: `getBlock(Tuple{Int,Int,Int})`
     * Returns the block present at the specified coordinates.
 
-While the `getBlock` and `setBlock` functions will accept `Float64` as arguments these will be rounded and set to `Int` as a block cannot be placed on non-integer coordinates.
+While the `getBlock` and `setBlock` functions will accept `Float64` as arguments but these will be rounded to `Int` as a block cannot be placed on non-integer coordinates.
+
+### Models
+
+A model is a 3-D Array of Blocks. Whenever we refer to a model we more specifically mean `Array{Block, 3}`. Storing a model can be convenient way to write scripts. For example if we have a streetlight stored as a model we can easily write a script to copy and paste at equal intervals on a street. There are model manipulation functions available such as:
+
+1) Copy Model: `copyModel(p1::Tuple{Real, Real, Real}, p2::Tuple{Real, Real, Real})`
+    * Copy the Blocks between the diagonally opposite blocks and store in a 3-D Block Array.
+2) Cut Model: `cutModel(p1::Tuple{Real, Real, Real}, p2::Tuple{Real, Real, Real}, block::Block)`
+    * Copy blocks between diagonally opposite blocks and fill the space with `block`.
+3) Flip Model: ```flip(A::Array{Block, 3}, dim::Symbol)```
+    * Flip a Block Array in a particular dimension. Dimensions are `x`, `y` and `z`
+4) Rotate Model
--- src/buildTools.jl
+++ src/buildTools.jl
@@ -18,7 +18,7 @@ end
 """
     copyModel(p1::Tuple{Real, Real, Real}, p2::Tuple{Real, Real, Real})
 
-Copy the Blocks between the diagonally opposite blocks in the form of a 3-D Block Array.
+Copy the Blocks between the diagonally opposite blocks and store in a 3-D Block Array.
 """
 function copyModel(p1::Tuple{Real, Real, Real}, p2::Tuple{Real, Real, Real})
     p1 = round.(Int, p1)
@@ -45,3 +45,138 @@ function cutModel(p1::Tuple{Real, Real, Real}, p2::Tuple{Real, Real, Real}, bloc
     setBlocks(p1, p2, block)
     return m
 end
+
+"""
+    flip(A::Array{Block, 3}, dim::Symbol)
+
+Flip a 3-D Block Array in a particular dimension. Dimensions are `x`, `y` and `z`
+
+# Example
+```julia-repl
+julia> A = reshape(Block.(collect(1:8)), (2,2,2))
+2×2×2 Array{PiCraft.Block,3}:
+[:, :, 1] =
+ PiCraft.Block(1, 0)  PiCraft.Block(3, 0)
+ PiCraft.Block(2, 0)  PiCraft.Block(4, 0)
+
+[:, :, 2] =
+ PiCraft.Block(5, 0)  PiCraft.Block(7, 0)
+ PiCraft.Block(6, 0)  PiCraft.Block(8, 0)
+
+julia> flip(A, :z)
+2×2×2 Array{PiCraft.Block,3}:
+[:, :, 1] =
+ PiCraft.Block(5, 0)  PiCraft.Block(7, 0)
+ PiCraft.Block(6, 0)  PiCraft.Block(8, 0)
+
+[:, :, 2] =
+ PiCraft.Block(1, 0)  PiCraft.Block(3, 0)
+ PiCraft.Block(2, 0)  PiCraft.Block(4, 0)
+```
+"""
+flip(A::Array{Block, 3}, dim::Symbol) = flip(A, Val{dim})
+
+function flip(A, ::Type{Val{:x}})
+    B = similar(A)
+    for i = 1:size(A, 1)
+        B[i, :, :] = A[size(A, 1) - i + 1, :, :]
+    end
+    return B
+end
+
+function flip(A, ::Type{Val{:y}})
+    B = similar(A)
+    for i = 1:size(A, 1)
+        B[:, i, :] = A[:, size(A, 1) - i + 1, :]
+    end
+    return B
+end
+
+function flip(A, ::Type{Val{:z}})
+    B = similar(A)
+    for i = 1:size(A, 1)
+        B[:, :, i] = A[:, :, size(A, 1) - i + 1]
+    end
+    return B
+end
+
+"""
+    rotate(A::Array{Block, 3}; dir::Symbol = :l, dim::Symbol = :x)
+
+Rotate a 3-D Block Array 90 degrees in a dimension and direction.
+Dimensions are `x`, `y` and `z`. Directions are `l` & `r`.
+
+# Example
+```julia-repl
+julia> A = reshape(Block.(collect(1:8)), (2,2,2))
+2×2×2 Array{PiCraft.Block,3}:
+[:, :, 1] =
+ PiCraft.Block(1, 0)  PiCraft.Block(3, 0)
+ PiCraft.Block(2, 0)  PiCraft.Block(4, 0)
+
+[:, :, 2] =
+ PiCraft.Block(5, 0)  PiCraft.Block(7, 0)
+ PiCraft.Block(6, 0)  PiCraft.Block(8, 0)
+
+julia> rotate(A, dir = :r, dim = :y)
+ 2×2×2 Array{PiCraft.Block,3}:
+ [:, :, 1] =
+  PiCraft.Block(3, 0)  PiCraft.Block(7, 0)
+  PiCraft.Block(4, 0)  PiCraft.Block(8, 0)
+
+ [:, :, 2] =
+  PiCraft.Block(1, 0)  PiCraft.Block(5, 0)
+  PiCraft.Block(2, 0)  PiCraft.Block(6, 0)
+```
+"""
+rotate(A::Array{Block, 3}, dir::Symbol, dim::Symbol) = rotate(A, Val{dir}, Val{dim})
+rotate(A::Array{Block, 3}; dir::Symbol = :l, dim::Symbol = :x) = rotate(A, Val{dir}, Val{dim})
+
+function rotate(A, ::Type{Val{:l}}, ::Type{Val{:x}})
+    B = similar(A, (size(A, 1), size(A, 3), size(A, 2)))
+    for i in 1:size(A, 1)
+        B[i,:,:] = rotl90(A[i,:,:])
+    end
+    return B
+end
+
+function rotate(A, ::Type{Val{:l}}, ::Type{Val{:y}})
+    B = similar(A, (size(A, 3), size(A, 2), size(A, 1)))
+    for i in 1:size(A, 2)
+        B[:, i, :] = rotl90(A[:, i, :])
+    end
+    return B
+end
+
+function rotate(A, ::Type{Val{:l}}, ::Type{Val{:z}})
+    B = similar(A, (size(A, 2), size(A, 1), size(A, 3)))
+    for i in 1:size(A, 1)
+        B[:, :, i] = rotl90(A[:, :, i])
+    end
+    return B
+end
+
+function rotate(A, ::Type{Val{:r}}, ::Type{Val{:y}})
+    B = similar(A, (size(A, 1), size(A, 3), size(A, 2)))
+    for i in 1:size(A, 1)
+        B[i,:,:] = rotr90(A[i,:,:])
+    end
+    return B
+end
+
+function rotate(A, ::Type{Val{:r}}, ::Type{Val{:z}})
+    B = similar(A, (size(A, 3), size(A, 2), size(A, 1)))
+    for i in 1:size(A, 1)
+        B[:, i, :] = rotr90(A[:, i, :])
+    end
+    return B
+end
+
+function rotate(A, ::Type{Val{:r}}, ::Type{Val{:x}})
+    B = similar(A, (size(A, 2), size(A, 1), size(A, 3)))
+    for i in 1:size(A, 1)
+        B[:, :, i] = rotr90(A[:, :, i])
+    end
+    return B
+end
+
