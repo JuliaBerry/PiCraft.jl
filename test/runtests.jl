756855a9e2b9943238e02ef6cf3fd26e51804f99
Ellipse0934 <adityapuranik97@gmail.com>
2018-12-30 21:01:28 +0530

    Add tests (#33)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,11 @@
-using PiCraft
-using Test
+using PiCraft, Test
 
-
-b=Block(PiCraft.AIR)
+@testset "NBT/schematic File reading" begin
+    @test parseNBT("../example/hello_world.nbt", devnull) == 0
+    @test parseNBT("../example/bigtest.nbt", devnull) == 0
+    m = Array{Block, 3}(undef, 3, 2, 4)
+    m[:, :, 1] = Block.([41 0; 41 0; 41 0])
+    m[:, :, 4] = m[:, :, 3] = m[:, :, 2] = m[:, :, 1]
+    m[3, 2, 3] = m[3, 2, 4] = Block(57)
+    @test importSchematic("../example/sample.schematic") == m
+end
